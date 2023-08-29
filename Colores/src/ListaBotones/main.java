902ec33b970eb873c01ee4772cc1615d8a00959c
Sergio Ortiz <65801936+SergioNAO@users.noreply.github.com>
2023-08-29 14:03:42 -0600

    Add files via upload

---
+++ Colores/src/ListaBotones/main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ListaBotones;
+
+import javax.swing.JFrame;
+
+public class main extends JFrame {
+    public static void main(String[] args){
+  
+      new Colores().setVisible(true);
+      
+    }
+
+   }
