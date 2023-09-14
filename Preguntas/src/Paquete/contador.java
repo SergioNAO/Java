466209d8f5f6910f0ea1be0f993eb8268b13c241
Sergio Ortiz <65801936+SergioNAO@users.noreply.github.com>
2023-09-14 11:09:58 -0600

    Add files via upload

---
+++ Preguntas/src/Paquete/contador.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Paquete;
+
+/**
+ *
+ * @author sergio
+ */
+public class contador {
+    int a,p1;
+    
+    
+    public contador(){
+    a =0;
+    p1=0;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getP1() {
+        return p1;
+    }
+
+    public void setP1(int p1) {
+        this.p1 = p1;
+    }
+    
+    
+    public void suma(){
+    
+    a++;
+    }
+    
+    
+}
